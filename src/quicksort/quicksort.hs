ec407ab96f8f09eed27eb4654aa99f05b94f9955
Alexander Elfimow <my.free.mail@gmx.de>
2017-08-13 23:25:50 +0200

    Added quisort function using filter

--- src/quicksort/quicksort.hs
+++ src/quicksort/quicksort.hs
@@ -4,3 +4,10 @@ quicksort (x : xs) =
     let smallerOrEqual = [ a | a <- xs, a <= x ]
         larger = [ a | a <- xs, a > x ]
     in quicksort smallerOrEqual ++ [x] ++ quicksort larger
+
+quicksort' :: (Ord a) => [a] -> [a]
+quicksort' [] = []
+quicksort' (x : xs) =
+    let smallerOrEqual = filter (<= x) xs
+        larger = filter (> x) xs
+    in quicksort' smallerOrEqual ++ [x] ++ quicksort' larger
