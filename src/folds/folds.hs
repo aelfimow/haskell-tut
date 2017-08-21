f8cf7b763952e18425ba39c7734af5949b83ae47
Alexander Elfimow <my.free.mail@gmx.de>
2017-08-21 22:51:23 +0200

    folds: added function

--- src/folds/folds.hs
+++ src/folds/folds.hs
@@ -3,3 +3,6 @@ reverse' = foldl (\acc x -> x : acc) []
 
 product' :: (Num a) => [a] -> a
 product' = foldl (*) 1
+
+filter' :: (a -> Bool) -> [a] -> [a]
+filter' p = foldr (\x acc -> if p x then x : acc else acc) []
