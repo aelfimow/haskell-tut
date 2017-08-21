2c6af982b9767e848acd22c366153def36c5c2e8
Alexander Elfimow <my.free.mail@gmx.de>
2017-08-21 23:00:46 +0200

    folds: added function

--- src/folds/folds.hs
+++ src/folds/folds.hs
@@ -6,3 +6,9 @@ product' = foldl (*) 1
 
 filter' :: (a -> Bool) -> [a] -> [a]
 filter' p = foldr (\x acc -> if p x then x : acc else acc) []
+
+last' :: [a] -> a
+last' = foldl1 (\_ x -> x)
+
+and' :: [Bool] -> Bool
+and' xs = foldr (&&) True xs
